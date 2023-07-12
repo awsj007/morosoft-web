51c421c94903f7e86b6e472c0c81d65fd8fe1d0a
Usama Iqbal <usamiqbal5@gmail.com>
2023-07-13 02:01:21 +0500

    Update header.jsx

--- components/Header/header.jsx
+++ components/Header/header.jsx
@@ -30,7 +30,7 @@ function header() {
                                 className="h-6 w-6"
                                 fill="none"
                                 viewBox="0 0 24 24"
-                                stroke-width="1.5"
+                                stroke-width="1.6"
                                 stroke="currentColor"
                                 aria-hidden="true"
                             >
