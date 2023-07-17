edead7e23622fa37bdc54cc378fd0e9e2a756e50
Usama Iqbal <usamiqbal5@gmail.com>
2023-07-17 23:01:27 +0500

    Header Updated

--- components/common/Header/page.jsx
+++ components/common/Header/page.jsx
@@ -2,7 +2,23 @@
 import React from "react";
 
 const Header = () => {
-    return <div></div>;
+    return (
+        <>
+            <header className="container-xl"> 
+                <section className="grid grid-cols-4 gap-4">
+                    <div className="">
+                        <h1>Logo</h1>
+                    </div>
+                    <nav className="">
+                        <h1>Menu</h1>
+                    </nav>
+                    <div className="">
+                        <h1>Search Icon</h1>
+                    </div>
+                </section>
+            </header>
+        </>
+    )
 };
 
 export default Header;
