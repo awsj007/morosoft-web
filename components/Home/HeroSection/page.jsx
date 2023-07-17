5063e648f46eda1d92563a26afe7d0d671ad9f5c
Awais Javed <awaisjaved55@gmail.com>
2023-07-17 12:48:03 +0500

    hero section

--- components/Home/HeroSection/page.jsx
+++ components/Home/HeroSection/page.jsx
@@ -4,17 +4,17 @@ import Image from "next/image";
 function HeroSection() {
     return (
         <div className="bg-hero-pattern  bg-dark-gray">
-            <div className="relative mx-auto max-w-7xl px-6 lg:px-8 flex items-center min-h-screen">
+            <div className="relative mx-auto max-w-7xl px-6 lg:px-8 flex items-center min-h-screen py-32">
                 <div className="mx-auto ">
                     <div className="text-left text-white">
                         <div className="relative ">
                             <img
                                 src="./assets/reverse-c.svg"
                                 alt=""
-                                className="h-[118px] w-[118px] absolute right-96 z-10"
+                                className="h-[118px] w-[118px] absolute right-[40%] z-10"
                             />
                             <img
-                                className="absolute right-[270px] top-12 z-0"
+                                className="absolute right-[27%] top-[60px] z-0"
                                 src="./assets/right-arrow.svg"
                                 alt=""
                             />
@@ -26,7 +26,7 @@ function HeroSection() {
                     </div>
 
                     <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                        <div className="mx-auto mt-10 grid grid-cols-1 gap-x-8 gap-y-16 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-4 place-items-center">
                             <div className="flex max-w-xl flex-col items-start justify-between">
                                 <div className="group relative">
                                     <p className="mt-5 line-clamp-3 text-sm leading-6 text-white">
@@ -35,64 +35,62 @@ function HeroSection() {
                                         application developments.
                                     </p>
                                     <img
-                                        className="py-12"
+                                        className="py-10"
                                         src="./assets/right-arrow.svg"
                                         alt=""
                                     />
-                                </div>
-                                <div className="relative flex items-center gap-x-4 text-white">
-                                    <img
-                                        src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                        alt=""
-                                        className="h-10 w-10 rounded-full bg-gray-50"
-                                    />
-                                    <div className="text-sm leading-6">
-                                        <p className="font-semibold">
-                                            <a href="#">
-                                                <span className="absolute inset-0"></span>
-                                                Michael Foster
-                                            </a>
-                                        </p>
-                                        <p>Co-Founder / CTO</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="flex max-w-xl flex-col items-start justify-between">
-                                <div className="group relative">
-                                    <p className="mt-5 line-clamp-3 text-base leading-6 text-white">
-                                        We are one of the best organization
-                                        which is rapidly growing in mobile
-                                        application developments.
-                                    </p>
-                                </div>
-                                <div className="relative mt-8 flex items-center gap-x-4 text-white">
-                                    <img
-                                        src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                        alt=""
-                                        className="h-10 w-10 rounded-full bg-gray-50"
-                                    />
-                                    <div className="text-sm leading-6">
-                                        <p className="font-semibold">
-                                            <a href="#">
-                                                <span className="absolute inset-0"></span>
-                                                Michael Foster
-                                            </a>
-                                        </p>
-                                        <p>Co-Founder / CTO</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="flex max-w-xl flex-col items-start justify-between">
-                                <div>
-                                    <button className="w-[128px] h-[128px] bg-primary-red text-white rounded-full">
+                                    <div className="flex flex-row text-white items-top ">
                                         <img
-                                            src="./assets/arrow.svg"
+                                            src="./assets/client-1.png"
                                             alt=""
-                                            className="w-5 h-5 mx-auto "
+                                            className="h-10 w-10 rounded-full"
                                         />
-                                        Explores
-                                    </button>
+                                        <img
+                                            src="./assets/client-2.png"
+                                            alt=""
+                                            className="h-10 w-10 rounded-full -ml-4"
+                                        />
+                                        <img
+                                            src="./assets/client-3.png"
+                                            alt=""
+                                            className="h-10 w-10 rounded-full -ml-4"
+                                        />
+                                        <img
+                                            src="./assets/client-4.png"
+                                            alt=""
+                                            className="h-10 w-10 rounded-full -ml-4"
+                                        />
+                                        <span className="h-10 w-12 bg-primary-red rounded-full -ml-4 flex flex-col items-center justify-center text-[25px] pb-1">
+                                            +
+                                        </span>
+                                        <div className="text-sm ml-2">
+                                            <p>100+ Popular Clients</p>
+                                        </div>
+                                    </div>
                                 </div>
+                            </div>
+                            <div className="col-span-2 place-items-center relative">
+                                <img
+                                    src="./assets/play.svg"
+                                    alt=""
+                                    className="absolute top-0 bottom-0 left-0 right-0  m-auto "
+                                />
+                                <img
+                                    src="./assets/video-thumbnail.png"
+                                    alt=""
+                                    srcset=""
+                                    className="w-[541px] h-[283px]"
+                                />
+                            </div>
+                            <div className="flex flex-col">
+                                <button className="w-[128px] h-[128px] bg-primary-red text-white rounded-full">
+                                    <img
+                                        src="./assets/arrow.svg"
+                                        alt=""
+                                        className="w-5 h-5 mx-auto "
+                                    />
+                                    Explores
+                                </button>
                             </div>
                         </div>
                     </div>
