6056656981ca73dae6ce04b4d880eed603e4cde8
Usama Iqbal <usamiqbal5@gmail.com>
2023-07-18 13:28:06 +0500

    header updated

--- components/common/Header/page.jsx
+++ components/common/Header/page.jsx
@@ -4,12 +4,12 @@ import React from "react";
 const Header = () => {
     return (
         <>
-            <header className="container-xl"> 
+            <header className="container mx-auto flex"> 
                 <section className="grid grid-cols-4 gap-4">
                     <div className="">
                         <h1>Logo</h1>
                     </div>
-                    <nav className="">
+                    <nav className="bg-black">
                         <h1>Menu</h1>
                     </nav>
                     <div className="">
