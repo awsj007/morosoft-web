b9e9e9cc0a9fc31e4d90cd066aea0a66be2678b5
Usama Iqbal <usamiqbal5@gmail.com>
2023-07-18 18:22:38 +0500

    Header Updated

--- components/common/Header/page.jsx
+++ components/common/Header/page.jsx
@@ -4,18 +4,46 @@ import React from "react";
 const Header = () => {
     return (
         <>
-            <header className="container mx-auto flex"> 
-                <section className="grid grid-cols-4 gap-4">
-                    <div className="">
-                        <h1>Logo</h1>
-                    </div>
-                    <nav className="bg-black">
-                        <h1>Menu</h1>
-                    </nav>
-                    <div className="">
-                        <h1>Search Icon</h1>
-                    </div>
-                </section>
+            <header className="relative bg-dark-gray text-white"> 
+                <div className=" left-0 right-0 w-[1200px] mx-auto ">
+                    <section className="grid grid-cols-3 gap-4 py-[35px]">
+                        <div className="">
+                            <img 
+                                src="./assets/logo.png"     
+                                alt=""
+                                className="w-[174px] h-[45px] object-contain"
+                            />
+                        </div>
+                        <div className="">
+                            <ul className="flex">
+                                <li>
+                                    <a href="/" className="text-sm font-medium text-primary-red pe-[30px]">Home</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="text-sm pe-[30px]">About Us</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="text-sm pe-[30px]">Services</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="text-sm pe-[30px]">Portfolio</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="text-sm pe-[30px]">Blogs</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="text-sm pe-[30px]">Hire</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="text-sm pe-[30px]">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="">
+                            <h1>Search Icon</h1>
+                        </div>
+                    </section>
+                </div>
             </header>
         </>
     )
